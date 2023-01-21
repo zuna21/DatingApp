735010b5a1c4896e6ae9a2533dd641d542e2b0ea
Adir Zunic <adir.zunic@gmail.com>
2023-01-21 14:14:02 +0100

    End of section 5

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  registerMode: boolean = false;
+
+  constructor() {}
+
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
+
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
+  }
+}
