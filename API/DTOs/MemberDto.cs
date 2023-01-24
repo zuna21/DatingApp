351ce86f69b89af7710741ee57ff5383ffd6c33e
Adir Zunic <adir.zunic@gmail.com>
2023-01-24 20:26:06 +0100

    End of section 8

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -5,6 +5,7 @@ namespace API.DTOs
         public int Id {get; set;}
         public string UserName {get; set;}     
         public int Age {get; set;}
+        public string PhotoUrl {get; set;}
         public string KnownAs {get; set;}
         public DateTime Created {get; set;}
         public DateTime LastActive {get; set;}
