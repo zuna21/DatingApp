250067438b194f517d8aae9e82b3e9869805367b
Adir Zunic <adir.zunic@gmail.com>
2023-01-25 21:25:26 +0100

    Routing to the detailed page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
+import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [
@@ -47,6 +48,11 @@ import { MemberCardComponent } from './members/member-card/member-card.component
     {
       provide: HTTP_INTERCEPTORS,
       useClass: ErrorInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
       multi: true
     },
   ],
