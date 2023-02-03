d320a722e0429a9cb0c2f2b55bbbc59cfb10e415
Adir Zunic <adir.zunic@gmail.com>
2023-02-03 23:09:47 +0100

    Adding custom validators

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { LoadingSpinnerService } from '../_services/loading-spinner.service';
 
@@ -7,23 +8,48 @@ import { LoadingSpinnerService } from '../_services/loading-spinner.service';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   @Output() cancleRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup = new FormGroup({});
 
   constructor(private accountService: AccountService,
       private loadingSpinnerService: LoadingSpinnerService
     ) {}
 
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('Hello', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
+    });
+
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity
+    });
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : {notMatching: true};
+    };
+  }
+
   register() {
-    this.loadingSpinnerService.setIsLoading(true);
+    console.log(this.registerForm.value);
+    console.log(this.registerForm.valid);
+    /* this.loadingSpinnerService.setIsLoading(true);
     this.accountService.register(this.model).subscribe({
       next: () => {
         this.cancel();
         this.loadingSpinnerService.setIsLoading(false);
       },
       error: error => console.log(error)
-    });
+    }); */
   }
 
   cancel() {
